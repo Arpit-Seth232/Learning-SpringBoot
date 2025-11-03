18717b290d6fb25f705541578b568f36d105c613
ArpitSeth-232 <arpitseth232@gmail.com>
2025-11-03 18:51:25 +0530

    make rest api

---
+++ src/main/java/com/example/demo/NotFoundResponseObject.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+public class NotFoundResponseObject{
+	private Long id;
+	private String message;
+
+	public void setId(Long id){
+		this.id = id;
+	}
+
+	public Long getId(){
+		return id;
+	}
+
+	public void setMessage(String message){
+		this.message = message;
+	}
+
+	public String getMessage(){
+		return message;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"NotFoundResponseObject{" + 
+			"id = '" + id + '\'' + 
+			",message = '" + message + '\'' + 
+			"}";
+		}
+}
---
+++ src/main/java/com/example/demo/TodoController.java
@@ -0,0 +1,94 @@
+package com.example.demo;
+import java.util.*;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/todos")
+public class TodoController {
+
+    public List<Todo>TodoList;
+
+    public TodoController() {
+        TodoList = new ArrayList<Todo>();
+        TodoList.add(new Todo(1, "dsa", false, 1));
+        TodoList.add(new Todo(2, "web", false, 2));
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Todo>> getTodos(){
+        return ResponseEntity.ok(TodoList);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Todo> createTodo(@RequestBody Todo newTodo){
+        TodoList.add(newTodo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newTodo);
+    }
+
+    @GetMapping("/{todoId}")
+    public ResponseEntity<?> getTodo(@PathVariable Long todoId){
+        for (Todo t : TodoList){
+            if(t.getId() == todoId){
+                return ResponseEntity.ok(t);
+            }
+        }
+        NotFoundResponseObject notFound = new NotFoundResponseObject();
+        notFound.setId(todoId);
+        notFound.setMessage("todo with id" + todoId + " !!! NOT FOUND !!!");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(notFound);
+    }
+
+    @DeleteMapping("/{todoId}")
+    public ResponseEntity<?> deleteTodo (@PathVariable Long todoId){
+        Todo delTodo = null;
+        for (Todo t : TodoList){
+            if(t.getId() == todoId){
+                delTodo = t;
+                TodoList.remove(t);
+            }
+        }
+
+        if(delTodo != null){
+            return ResponseEntity.ok(delTodo);
+        }
+        else{
+            NotFoundResponseObject notFound = new NotFoundResponseObject();
+            notFound.setId(todoId);
+            notFound.setMessage("todo with id" + todoId + " !!! NOT FOUND !!!");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(notFound);
+        }
+
+    }
+
+    @PatchMapping("")
+    public ResponseEntity<?> updateTodo(@RequestBody Todo updatedTodo){
+
+        boolean updated = false;
+
+        for (Todo t : TodoList){
+            if(t.getId() == updatedTodo.getId()){
+                TodoList.remove(t);
+                TodoList.add(updatedTodo);
+                updated = true;
+                break;
+            }
+        }
+
+        if(updated){
+            return ResponseEntity.ok(updatedTodo);
+        }
+        else{
+            long id = updatedTodo.getId();
+            NotFoundResponseObject notFound = new NotFoundResponseObject();
+            notFound.setId(id);
+            notFound.setMessage("todo with id" + id + " !!! NOT FOUND !!!");
+            return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(notFound);
+        }
+
+    }
+
+
+}
